114a2cf01509a962f56c132b1528a8a84fcf9274
berlianishma08 <berlianishma08@gmail.com>
2025-09-22 21:11:19 +0700

    feat: page Input data

--- app/input-data/page.tsx
+++ app/input-data/page.tsx
@@ -1,16 +1,30 @@
 import Navbar from '../components/navbar';
 import Footer from '../components/footer';
+import { NextPage } from 'next';
+import Head from 'next/head';
+import InputDataForm from './components/InputDataForm';
 
-const InputData = () => {
+const InputData: NextPage = () => {
     return(
-        <div className="flex flex-col min-h-screen">
-      <Navbar />
-      <main className="flex-1 p-6">
-        <h1 className="text-2xl font-bold">Input Data</h1>
-        <p>Input data anda.</p>
-      </main>
-      <Footer />
-    </div>
+        <>
+            <Head>
+                <title>Input Data - PT. Media Realindo Express</title>
+                <meta name="description" content="Input data sistem pengiriman" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+            </Head>
+            
+            <div className="flex flex-col min-h-screen">
+                <Navbar />
+                <main className="flex-1 p-6">
+                    <div className="flex flex-col min-h-screen">
+                        <h1 className="text-2xl font-bold mb-4">Input Data</h1>
+                        <p className="text-gray-600 mb-8">Input data pengiriman anda.</p>
+                        <InputDataForm/>
+                    </div>
+                </main>
+                <Footer />
+            </div>
+        </>
     );
 }
 
